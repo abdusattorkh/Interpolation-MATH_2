986ba93bb2825dd170855355e3b08da091452a46
luca-qu <81464390+luca-qu@users.noreply.github.com>
2023-04-17 11:28:21 +0200

    Fixed missing test function. Added x/y limits for plot

--- test.py
+++ test.py
@@ -19,17 +19,4 @@ def main(x,y):
 
 
     
-def test():
-    xy_intervall = [0,1] ##CHANGABLE INTERVALL
-    amount_dtp=30 ##CHANGABLE DTP AMOUNT
-    x = np.linspace(xy_intervall[0],xy_intervall[1],amount_dtp)
-    #x= np.empty(amount_dtp,dtype=float) ##CHANGABLE DATATYPE
 
-    y = np.empty(amount_dtp,dtype=float) ##CHANGABLE DATATYPE
-    for i,ele in enumerate(x):
-        y[i]=math.cos(ele*math.pi/2) ##CHANGABLE FUNCTION
-    print(y.size,x.size)
-    time=timeit.timeit(lambda:main(x,y),number=1)
-
-if __name__=="__main__":
-    test()
